6d804f767db909cfaf01f7a92fd9895911cf5e10
Tim Jones <Tim.Jones@vu.com>
2023-01-23 09:44:09 -0600

    Pay Calc

--- payCalculator.py
+++ payCalculator.py
@@ -3,6 +3,9 @@ def calculatePay():
     print("calculating pay")
     # This first line is provided for you
     hrs = input("Enter Hours:")
+    rate = input("Enter Rate:")
+    print(hrs * rate)
+
 
     # end assignment
 
